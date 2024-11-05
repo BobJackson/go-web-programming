156af4cb11819f582386b9ae4a5d65ef3fbb3533
Bob Jackson <wangyousong@9191offer.com>
2024-11-05 11:22:20 +0800

    Revert "ˆ"
    This reverts commit d9b1b5c2ffec4b1ee2a041a0f55775439892029a.

--- chapter06/store.go
+++ chapter06/store.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 type Post struct {
@@ -25,66 +23,7 @@ func main() {
 
 	//storeInMemory()
 
-	//storeToFile()
-
-	storeToCsv()
-}
-
-func storeToCsv() {
-	csvFile, err := os.Create("posts.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer func(csvFile *os.File) {
-		err := csvFile.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(csvFile)
-
-	allPosts := []Post{
-		Post{Id: 1, Content: "Hello World", Author: "John"},
-		Post{Id: 2, Content: "Bonjour Monde", Author: "Pierre"},
-		Post{Id: 3, Content: "Hola Mundo", Author: "Pedro"},
-		Post{Id: 4, Content: "Ciao mondo", Author: "Marco"},
-	}
-
-	write := csv.NewWriter(csvFile)
-
-	for _, post := range allPosts {
-		line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
-		err := write.Write(line)
-		if err != nil {
-			panic(err)
-		}
-	}
-	write.Flush()
-
-	file, err := os.Open("posts.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(file)
-
-	reader := csv.NewReader(file)
-	reader.FieldsPerRecord = -1
-	records, err := reader.ReadAll()
-	if err != nil {
-		panic(err)
-	}
-	var posts []Post
-	for _, item := range records {
-		id, _ := strconv.Atoi(item[0])
-		posts = append(posts, Post{Id: id, Content: item[1], Author: item[2]})
-	}
-	fmt.Println(posts[0].Id)
-	fmt.Println(posts[0].Content)
-	fmt.Println(posts[0].Author)
+	storeToFile()
 }
 
 func storeToFile() {
