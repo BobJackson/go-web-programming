ee17d21999faa2088697da33f9adf8671f959a1b
Bob Jackson <wangyousong@9191offer.com>
2024-11-06 09:36:50 +0800

    feat(chapter06): 添加评论功能并更新数据模型
    - 在 Post 结构中添加 Comments 字段，用于存储评论
    - 新增 Comment 结构体，包含评论相关内容
    - 实现 Comment 的 Create 方法，用于创建新评论
    - 更新 GetPost 方法，以获取并返回评论
    - 新增 schema.sql 文件，定义数据库表结构

--- chapter06/listing66/curd.go
+++ chapter06/listing66/curd.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/lib/pq"
 )
 
 type Post struct {
+	Id       int
+	Content  string
+	Author   string
+	Comments []Comment
+}
+
+type Comment struct {
 	Id      int
 	Content string
 	Author  string
+	Post    *Post
 }
 
 var Db *sql.DB
@@ -20,6 +29,20 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (comment *Comment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("post not found")
+		return
+	}
+	err = Db.QueryRow(
+		"insert into comments (content, author, post_id) values ($1, $2, $3) returning id",
+		comment.Content,
+		comment.Author,
+		comment.Post.Id,
+	).Scan(&comment.Id)
+	return
 }
 
 func Posts(limit int) (posts []Post, err error) {
@@ -57,7 +80,22 @@ func (post *Post) Create() (err error) {
 
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
+	post.Comments = []Comment{}
 	err = Db.QueryRow("select id, content, author from posts where id=$1", id).Scan(&post.Id, &post.Content, &post.Author)
+
+	rows, err := Db.Query("select id, content, author from comments where post_id=$1", id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	_ = rows.Close()
 	return
 }
 
@@ -73,20 +111,14 @@ func (post *Post) Delete() (err error) {
 
 func main() {
 	post := Post{Content: "Hello World!", Author: "John Doe"}
-	fmt.Println(post)
-
 	_ = post.Create()
-	fmt.Println(post)
+
+	comment := Comment{Content: "Nice post!", Author: "Jane Doe", Post: &post}
+	_ = comment.Create()
 
 	readPost, _ := GetPost(post.Id)
+
 	fmt.Println(readPost)
-
-	readPost.Content = "New Content"
-	readPost.Author = "New Author"
-	_ = readPost.Update()
-
-	posts, _ := Posts(10)
-	fmt.Println(posts)
-
-	_ = readPost.Delete()
+	fmt.Println(readPost.Comments)
+	fmt.Println(readPost.Comments[0].Post)
 }
