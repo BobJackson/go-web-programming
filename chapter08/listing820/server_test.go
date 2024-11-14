7647f666ec8d2e6e85c08a48941dddbb1d1544f9
Bob Jackson <wangyousong@9191offer.com>
2024-11-14 17:32:20 +0800

    test(server): 添加 PostTestSuite测试用例
    - 新增 PostTestSuite 结构体和相关测试方法
    - 实现 SetUpTest 和 TearDownTest 方法，用于测试前后的环境准备和清理
    - 添加 SetUpSuite 和 TearDownSuite 方法，用于测试套件的初始化和结束处理
    - 增加了对 GET 和 PUT 请求的测试用例

--- chapter08/listing820/server_test.go
+++ chapter08/listing820/server_test.go
@@ -5,10 +5,14 @@ import (
 	. "gopkg.in/check.v1"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 type PostTestSuite struct {
+	mux    *http.ServeMux
+	post   *FakePost
+	writer *httptest.ResponseRecorder
 }
 
 type FakePost struct {
@@ -42,6 +46,13 @@ func Test(t *testing.T) {
 	TestingT(t)
 }
 
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.post = &FakePost{}
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/post/", handleRequest(s.post))
+	s.writer = httptest.NewRecorder()
+}
+
 func (s *PostTestSuite) TestHandleGet(c *C) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
@@ -53,4 +64,37 @@ func (s *PostTestSuite) TestHandleGet(c *C) {
 	var post Post
 	_ = json.Unmarshal(writer.Body.Bytes(), &post)
 	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestGetPost(c *C) {
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	var post Post
+	_ = json.Unmarshal(s.writer.Body.Bytes(), &post)
+
+	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestPutPost(c *C) {
+	body := strings.NewReader(`{"content":"Updated post", "author": "Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", body)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	c.Check(s.post.Id, Equals, 1)
+	c.Check(s.post.Content, Equals, "Updated post")
+}
+
+func (s *PostTestSuite) TearDownTest(c *C) {
+	c.Log("Finished test - ", c.TestName())
+}
+
+func (s *PostTestSuite) SetUpSuite(c *C) {
+	c.Log("Starting Post Test Suite")
+}
+
+func (s *PostTestSuite) TearDownSuite(c *C) {
+	c.Log("Finished Post Test Suite")
 }
