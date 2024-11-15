a80c88dcd14bff1e6c9d798227c8a59113fa7ce1
Bob Jackson <wangyousong@9191offer.com>
2024-11-15 13:20:30 +0800

    refactor(chapter09):调整 goroutine 示例代码- 修改 printNumbers2 函数，将循环次数从 10增加到 100 - 修改 printLetters2 函数，将循环次数从 10 增加到 100 - 注释掉两个函数中的打印语句 -调整 printLetters1 函数的位置，使其与其他函数分离

--- chapter09/goroutine.go
+++ chapter09/goroutine.go
@@ -11,23 +11,23 @@ func printNumbers1() {
 	}
 }
 
-func printNumbers2() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d ", i)
-	}
-}
-
 func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
 		fmt.Printf("%c ", i)
 	}
 }
 
-func printLetters2() {
-	for i := 'A'; i < 'A'+10; i++ {
+func printNumbers2() {
+	for i := 0; i < 100; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%c ", i)
+		//fmt.Printf("%d ", i)
+	}
+}
+
+func printLetters2() {
+	for i := 'A'; i < 'A'+100; i++ {
+		time.Sleep(1 * time.Microsecond)
+		//fmt.Printf("%c ", i)
 	}
 }
 
