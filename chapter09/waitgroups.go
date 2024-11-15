97c7d6644d12485c198baca2f60ef8326dfc8f0a
Bob Jackson <wangyousong@9191offer.com>
2024-11-15 13:51:18 +0800

    feat(chapter09): 添加使用缓冲通道的示例并实现信道选择
    - 新增 usingBufferedChannel 函数，演示如何使用缓冲通道
    - 增加 callerA 和 callerB 函数，用于向通道发送消息
    - 在 main 函数中实现信道选择，接收来自两个通道的消息
    - 添加默认情况处理，展示 select语句的完整用法

--- chapter09/waitgroups.go
+++ chapter09/waitgroups.go
@@ -67,12 +67,37 @@ func catcher(c chan int) {
 	}
 }
 
-func main() {
-	//usingWaitGroup()
-	//usingChannel()
-
+func usingBufferedChannel() {
 	c := make(chan int, 3)
 	go thrower(c)
 	go catcher(c)
 	time.Sleep(100 * time.Microsecond)
+}
+
+func callerA(c chan string) {
+	c <- "Hello World!"
+}
+
+func callerB(c chan string) {
+	c <- "Hola Mundo!"
+}
+
+func main() {
+	//usingWaitGroup()
+	//usingChannel()
+	//usingBufferedChannel()
+	a, b := make(chan string), make(chan string)
+	go callerA(a)
+	go callerB(b)
+	for i := 0; i < 5; i++ {
+		//time.Sleep(1 * time.Microsecond)
+		select {
+		case msg := <-a:
+			fmt.Printf("%s from A\n", msg)
+		case msg := <-b:
+			fmt.Printf("%s from B\n", msg)
+		default:
+			fmt.Println("Default")
+		}
+	}
 }
